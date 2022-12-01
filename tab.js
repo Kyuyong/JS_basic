d2f3ad40b0b25abfabb0c2bebc5cd4e08b0e58f5
조규용 <mvpcouple@naver.com>
2022-12-01 15:05:57 +0900

    탭버튼 옮기는 법 여러가지 방법

--- tab.js
+++ tab.js
@@ -1,39 +1,37 @@
-//버튼0을 누르면 
-//모든 버튼에 orange show 제거
-//버튼0에 orange 클래스 부착
-//버튼0 show 부착
 var num = $('li').length;
 
-for (let i = 0; i < num; i++) {
-  $('.tab-button').eq(i).on('click', function () {
-    $('.tab-button').removeClass('orange');
-    $('.tab-content').removeClass('show');
-    $('.tab-button').eq(i).addClass('orange');
-    $('.tab-content').eq(i).addClass('show');
-  });
+
+//HTML에 Data id -> dataset 를 숨겨놓고, 작업을 하면 if문 없이 간단히 작업 가능
+$('.list').click(function (e) {
+
+  opentab(e.target.dataset.id)
+});
+
+
+// 오픈탭 함수가 있으면 if문으로도 쓸수 있음 
+//$('.list').click(function (e) {
+//  if (e.target == document.querySelectorAll('.tab-button')[0]) {
+//    opentab(0)
+//  }
+//  if (e.target == document.querySelectorAll('.tab-button')[1]) {
+//    opentab(1)
+//  }
+//  if (e.target == document.querySelectorAll('.tab-button')[2]) {
+//    opentab(2)
+//  }
+//});
+
+
+// 확장성을 위해, 탭버튼을 함수화 -> oepntab 함수를 만들어서 씀
+//for (let i = 0; i < num; i++) {
+//  $('.tab-button').eq(i).on('click', function () {
+//    opentab(i);
+//  })
+//};
+
+function opentab(e) {
+  $('.tab-button').removeClass('orange');
+  $('.tab-content').removeClass('show');
+  $('.tab-button').eq(e).addClass('orange');
+  $('.tab-content').eq(e).addClass('show');
 };
-//
-//
-//$('.tab-button').eq(0).on('click', function () {
-//  $('.tab-button').removeClass('orange');
-//  $('.tab-content').removeClass('show');
-//  $('.tab-button').eq(0).addClass('orange');
-//  $('.tab-content').eq(0).addClass('show');
-//
-//});
-//
-//$('.tab-button').eq(1).on('click', function () {
-//  $('.tab-button').removeClass('orange');
-//  $('.tab-content').removeClass('show');
-//  $('.tab-button').eq(1).addClass('orange');
-//  $('.tab-content').eq(1).addClass('show');
-//
-//});
-//
-//$('.tab-button').eq(2).on('click', function () {
-//  $('.tab-button').removeClass('orange');
-//  $('.tab-content').removeClass('show');
-//  $('.tab-button').eq(2).addClass('orange');
-//  $('.tab-content').eq(2).addClass('show');
-//
-//});
