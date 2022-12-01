5b0183e7b87dba9706f2c464bf86f1e7b12ed686
조규용 <mvpcouple@naver.com>
2022-12-01 10:13:21 +0900

    for문 활용한 버튼탭 이동

--- tab.js
+++ tab.js
@@ -2,29 +2,38 @@
 //모든 버튼에 orange show 제거
 //버튼0에 orange 클래스 부착
 //버튼0 show 부착
-var 버튼 = $('.tab-button');
+var num = $('li').length;
 
-
-$('.tab-button').eq(0).on('click', function () {
-  $('.tab-button').removeClass('orange');
-  $('.tab-content').removeClass('show');
-  $('.tab-button').eq(0).addClass('orange');
-  $('.tab-content').eq(0).addClass('show');
-
-});
-
-$('.tab-button').eq(1).on('click', function () {
-  $('.tab-button').removeClass('orange');
-  $('.tab-content').removeClass('show');
-  $('.tab-button').eq(1).addClass('orange');
-  $('.tab-content').eq(1).addClass('show');
-
-});
-
-$('.tab-button').eq(2).on('click', function () {
-  $('.tab-button').removeClass('orange');
-  $('.tab-content').removeClass('show');
-  $('.tab-button').eq(2).addClass('orange');
-  $('.tab-content').eq(2).addClass('show');
-
-});
+for (let i = 0; i < num; i++) {
+  $('.tab-button').eq(i).on('click', function () {
+    $('.tab-button').removeClass('orange');
+    $('.tab-content').removeClass('show');
+    $('.tab-button').eq(i).addClass('orange');
+    $('.tab-content').eq(i).addClass('show');
+  });
+};
+//
+//
+//$('.tab-button').eq(0).on('click', function () {
+//  $('.tab-button').removeClass('orange');
+//  $('.tab-content').removeClass('show');
+//  $('.tab-button').eq(0).addClass('orange');
+//  $('.tab-content').eq(0).addClass('show');
+//
+//});
+//
+//$('.tab-button').eq(1).on('click', function () {
+//  $('.tab-button').removeClass('orange');
+//  $('.tab-content').removeClass('show');
+//  $('.tab-button').eq(1).addClass('orange');
+//  $('.tab-content').eq(1).addClass('show');
+//
+//});
+//
+//$('.tab-button').eq(2).on('click', function () {
+//  $('.tab-button').removeClass('orange');
+//  $('.tab-content').removeClass('show');
+//  $('.tab-button').eq(2).addClass('orange');
+//  $('.tab-content').eq(2).addClass('show');
+//
+//});
